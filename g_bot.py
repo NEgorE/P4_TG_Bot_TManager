1c1039fa9d8f87e125b3c6b4f1d0d39e58593253
neliubin.egor <neo.8318731@gmail.com>
2023-04-18 15:19:47 +0300

    b1 - p3

--- g_bot.py
+++ g_bot.py
@@ -1,18 +1,27 @@
 import telebot
-
 from token_str import token
-bot = telebot.TeleBot(token)
-
+import g_sqlA as dbc
 from g_bot_msg_lib import msg_text as mt
-lang = 'en'
 
-@bot.message_handler(commands=["start"])
-def start(msg) :
-    print(msg.from_user.id, msg.from_user.username, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.language_code)
-    bot.send_message(msg.chat.id, mt[lang]['str1'] % {'user_first_name' : msg.from_user.first_name})
+def main():
+    
+    db_str = 'sqlite:///sqlite3.db'
+    db_con = dbc.DBclass(db_str)
 
+    lang = 'en'
 
+    bot = telebot.TeleBot(token)
 
+    @bot.message_handler(commands=["start"])
+    def start(msg) :
+        print(msg.from_user.id, msg.from_user.username, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.language_code)
+        bot.send_message(msg.chat.id, mt[lang]['str1'] % {'user_first_name' : msg.from_user.first_name})
 
+    @bot.message_handler(commands=["help"])
+    def help(msg) :
+        bot.send_message(msg.chat.id, mt[lang]['help'])
 
-bot.polling(none_stop = True)
+    bot.polling(none_stop = True)
+
+if __name__ == '__main__':
+    main()
--- g_sqlA.py
+++ g_sqlA.py
@@ -1,21 +1,37 @@
 import sqlalchemy as sqlA
 import datetime
 
-DB_engine = sqlA.create_engine('sqlite:///sqlite3.db')
-DB_metadata = sqlA.MetaData()
+class DBclass :
 
-tasks = sqlA.Table('tasks', DB_metadata,
-                   sqlA.Column('id' , sqlA.Integer(), primary_key=True),
-                   sqlA.Column('created' , sqlA.DateTime(), default=datetime.datetime.now()),
-                   sqlA.Column('user_id' , sqlA.Integer(), nullable=False),
-                   sqlA.Column('date' , sqlA.Date(), nullable=False),
-                   sqlA.Column('time' , sqlA.Time(), nullable=False),
-                   sqlA.Column('text' , sqlA.String(100), nullable=False),
-                   sqlA.Column('status' , sqlA.String(4), nullable=False)
-)
+    def __init__(self, db_str) -> None:
+        
+        DB_engine = sqlA.create_engine(db_str)
+        DB_metadata = sqlA.MetaData()
 
-scheduler = sqlA.Table('scheduler', DB_metadata,
-                       sqlA.Column('id' , sqlA.Integer(), primary_key=True, autoincrement=True),
-                       sqlA.Column('task_id' , sqlA.Integer(), nullable=False),
-                       sqlA.Column('time' , sqlA.Time(), nullable=False)
-)
+        tasks = sqlA.Table('tasks', DB_metadata,
+                        sqlA.Column('id' , sqlA.Integer(), primary_key=True),
+                        sqlA.Column('created' , sqlA.DateTime(), default=datetime.datetime.now()),
+                        sqlA.Column('user_id' , sqlA.Integer(), nullable=False),
+                        sqlA.Column('date' , sqlA.Date(), nullable=False),
+                        sqlA.Column('time' , sqlA.Time(), nullable=False),
+                        sqlA.Column('text' , sqlA.String(100), nullable=False),
+                        sqlA.Column('status' , sqlA.String(4), nullable=False)
+        )
+        scheduler = sqlA.Table('scheduler', DB_metadata,
+                            sqlA.Column('id' , sqlA.Integer(), primary_key=True, autoincrement=True),
+                            sqlA.Column('task_id' , sqlA.Integer(), nullable=False),
+                            sqlA.Column('time' , sqlA.Time(), nullable=False)
+        )
+        users = sqlA.Table('users', DB_metadata,
+                            sqlA.Column('id' , sqlA.Integer(), primary_key=True, autoincrement=True),
+                            sqlA.Column('username' , sqlA.Integer(), nullable=False),
+                            sqlA.Column('last_name' , sqlA.String(20), nullable=True),
+                            sqlA.Column('first_name' , sqlA.String(120), nullable=False),
+                            sqlA.Column('language_code' , sqlA.String(3), nullable=True)
+        )
+
+        DB_metadata.create_all(DB_engine)
+        print('DB init done!!!')
+
+    def __del__(self) :
+        print('DB class was delete !!!')
